e7217395a571e19bab9e53a7a3480abc5caea1d9
Eduard Pavliuc <epavliuc@spartaglobal.com>
2019-10-21 16:21:50 +0100

    Initial commit

---
+++ RedditTestProject/Pages/RedditHome.cs
@@ -0,0 +1,95 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditTestProject.Pages
+{
+    class RedditHome
+    {
+        private readonly IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='SHORTCUT_FOCUSABLE_DIV']/div[1]/header/div/div[2]/div[2]/div[1]/a[1]")]
+        private IWebElement loginBtn;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div/div[2]/div/form/fieldset[5]/button")]
+        private IWebElement signInBtn;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='SHORTCUT_FOCUSABLE_DIV']/div[2]/div/div/div/div[2]/div[5]/div[1]/div/div[2]")]
+        private IWebElement profileMessage;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div/div/div[2]/div/form/fieldset[2]/div")]
+        private IWebElement invalidPMessage;
+
+
+        
+
+
+        [FindsBy(How = How.Id, Using = "loginUsername")]
+        private IWebElement loginUsername;
+
+        [FindsBy(How = How.Id, Using = "loginPassword")]
+        private IWebElement loginPassword;
+
+
+        
+
+
+        public RedditHome(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.reddit.com/");
+        }
+
+        public void LoginBtn()
+        {
+            loginBtn.Click();
+        }
+
+        public void UsernameInput()
+        {
+            loginUsername.SendKeys("epSparta");
+        }
+
+        public void ValidPasswordInput()
+        {
+            loginPassword.SendKeys("Sparta2019!");
+        }
+
+        public void InvalidPasswordInput()
+        {
+            loginPassword.SendKeys("Sparta201");
+        }
+
+        public void SignIn()
+        {
+            signInBtn.Click();
+        }
+
+
+            public void ClickDropDownProfile()
+            {
+                IWebElement dropDown = driver.FindElement(By.XPath("/html/body/div[6]/div"));
+                IList<IWebElement> dropDownList = dropDown.FindElements(By.ClassName("_1YWXCINvcuU7nk0ED-bta8"));
+                dropDownList[0].Click();
+            }
+
+        public string ProfileMessage()
+        {
+            return profileMessage.Text;
+        }
+
+        public string InvalidPassword()
+        {
+            return invalidPMessage.Text;
+        }
+    }
+}
---
+++ RedditTestProject/Steps/LoginSteps.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using RedditTestProject.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace RedditTestProject.Pages
+{
+    [Binding]
+    public class LoginSteps
+    {
+        IWebDriver driver;
+        RedditHome page;
+
+        [Scope(Feature = "Login")]
+        [BeforeScenario]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            page = new RedditHome(driver);
+        }
+
+
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            page.GoToPage();
+        }
+        
+        [Given(@"I click on the login button")]
+        public void GivenIClickOnTheLoginButton()
+        {
+            page.LoginBtn();
+        }
+        
+        [Given(@"Input valid details")]
+        public void GivenInputValidDetails()
+        {
+            page.UsernameInput();
+            page.ValidPasswordInput();
+        }
+        
+        [Given(@"I click on the sign in button")]
+        public void GivenIClickOnTheSignInButton()
+        {
+            page.SignIn();
+        }
+        
+        [Given(@"Input invalid password")]
+        public void GivenInputInvalidPassword()
+        {
+            page.InvalidPasswordInput();
+        }
+        
+        [Then(@"I should see the username on the homepage")]
+        public void ThenIShouldSeeTheUsernameOnTheHomepage()
+        {
+            page.ClickDropDownProfile();
+            Assert.IsTrue(page.ProfileMessage().Contains("epSparta"));
+           
+        }
+        
+        [Then(@"I should see the correct error message")]
+        public void ThenIShouldSeeTheCorrectErrorMessage()
+        {
+            Assert.AreEqual("Incorrect password", page.ProfileMessage());
+        }
+
+        [Scope(Feature = "Login")]
+        [AfterScenario]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
